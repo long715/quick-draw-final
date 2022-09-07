b7a0151a2d978d7c1777853d9c4afb55af0c8778
Bloooey <bluey17501@gmail.com>
2022-09-07 19:07:31 +1200

    [refac] Save menu changed to be in modal window

--- src/main/java/nz/ac/auckland/se206/CanvasController.java
+++ src/main/java/nz/ac/auckland/se206/CanvasController.java
@@ -6,14 +6,8 @@ import ai.djl.translate.TranslateException;
 import com.opencsv.exceptions.CsvException;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
@@ -28,18 +22,19 @@ import javafx.beans.property.SimpleIntegerProperty;
 import javafx.concurrent.Task;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import javafx.util.Duration;
 
-import javax.imageio.ImageIO;
-import javax.swing.filechooser.FileSystemView;
 import nz.ac.auckland.se206.ml.DoodlePrediction;
 import nz.ac.auckland.se206.speech.TextToSpeech;
 import nz.ac.auckland.se206.words.CategorySelector;
@@ -62,14 +57,12 @@ public class CanvasController {
   @FXML private Canvas canvas;
   @FXML private Label lblCategory;
   @FXML private Label lblTime;
-  @FXML private Label lblGuesses;
   @FXML private Label lblWinOrLose;
+  @FXML private Label lblGuesses;
   @FXML private Button btnToMenu;
   @FXML private Button btnReady;
   @FXML private Button clearButton;
   @FXML private Button btnSaveDrawing;
-  @FXML private TextField txtDirectory;
-  @FXML private TextField txtFileName;
 
   private GraphicsContext graphic;
   private DoodlePrediction model;
@@ -115,10 +108,6 @@ public class CanvasController {
     btnSaveDrawing.setDisable(
         true); // user can't save an empty canvas, drawing can only be saved after game ends
 
-    // since the user can't save, they shouldn't be able to type into the text boxes before and
-    // during the game
-    txtDirectory.setDisable(true);
-    txtFileName.setDisable(true);
 
     model = new DoodlePrediction();
     speech = new TextToSpeech();
@@ -179,7 +168,8 @@ public class CanvasController {
                 FutureTask<StringBuilder> predict =
                     new FutureTask<StringBuilder>(
                         new Callable<StringBuilder>() {
-                          public StringBuilder call() throws TranslateException {
+                          public StringBuilder call() throws TranslateException {                          
+                           
                             // get the list of the top 10 classifications and format the list into
                             // stringbuilder
                             return DoodlePrediction.getPredictionString(
@@ -235,8 +225,6 @@ public class CanvasController {
           // allow user to save the current drawing and write on the text fields for
           // custom directory and file name inputs
           btnSaveDrawing.setDisable(false);
-          txtDirectory.setDisable(false);
-          txtFileName.setDisable(false);
 
           // update the winOrLose label and use the text to speech to tell the user if the they have
           // won or lost
@@ -272,7 +260,6 @@ public class CanvasController {
               bgLoseSpeech.start();
             }
           } catch (InterruptedException | ExecutionException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
           }
         });
@@ -327,36 +314,26 @@ public class CanvasController {
   @FXML
   private void onSave() throws IOException {
 
-    // set the default values for the directory and name, use desktop as the default directory
-    String directory =
-        FileSystemView.getFileSystemView().getHomeDirectory().getAbsolutePath() + "/desktop";
-    String name = "default";
+      // create new loader to load save menu modal
+    FXMLLoader loader = new FXMLLoader();
+    loader.setLocation(getClass().getResource("/fxml/" + "savemenu" + ".fxml"));
+    Parent root = loader.load();
 
-    // check if any of the text fields are filled up, if filled up, overwrite the default values
-    if (!txtDirectory.getText().equalsIgnoreCase("")) {
-      directory = txtDirectory.getText();
-    }
-    if (!txtFileName.getText().equalsIgnoreCase("")) {
-      name = txtFileName.getText();
-    }
+    //configure the modal
+    Stage stage = new Stage();
+    stage.setTitle("Save Menu");
+    stage.initModality(Modality.WINDOW_MODAL);
+    stage.initOwner(btnReady.getScene().getWindow());
+    stage.setScene(new Scene(root));
 
-    // create a file for the canvas drawing
-    File createdFile = saveCurrentSnapshotOnFile();
+    //use instance methods of the controller to pass in the current snapshot
+    SaveMenuController controller = loader.getController();
+    controller.setImage(getCurrentSnapshot());
+    controller.setStage(stage);
 
-    // this file input stream instance reads the created file into
-    // bytes so that the file output stream can write it
-    FileInputStream fis = new FileInputStream(createdFile);
-    byte[] arr = fis.readAllBytes();
-    fis.close();
+    //show the modal
+    stage.showAndWait();
 
-    // create the file path for the saved canvas drawing
-    Path newFilePath = Paths.get(directory + "/" + name + ".bmp");
-    Files.createFile(newFilePath); // create a file in this file path
-
-    // initialise the output stream and write on the created file
-    FileOutputStream fos = new FileOutputStream(directory + "/" + name + ".bmp");
-    fos.write(arr);
-    fos.close();
   }
 
   /**
@@ -401,40 +378,19 @@ public class CanvasController {
   }
 
   /**
-   * Save the current snapshot on a bitmap file.
-   *
-   * @return The file of the saved image.
-   * @throws IOException If the image cannot be saved.
-   */
-  private File saveCurrentSnapshotOnFile() throws IOException {
-    // You can change the location as you see fit.
-    final File tmpFolder = new File("tmp");
-
-    if (!tmpFolder.exists()) {
-      tmpFolder.mkdir();
-    }
-
-    // We save the image to a file in the tmp folder.
-    final File imageToClassify =
-        new File(tmpFolder.getName() + "/snapshot" + System.currentTimeMillis() + ".bmp");
-
-    // Save the image to a file.
-    ImageIO.write(getCurrentSnapshot(), "bmp", imageToClassify);
-
-    return imageToClassify;
-  }
-
-  /**
    * code adapted from
    * https://asgteach.com/2011/10/javafx-animation-and-binding-simple-countdown-timer-2/#:~:text=To%20start%20the%20timer%2C%20you,15%20and%20restarts%20the%20countdown.
    *
-   * <p>Starts the timer time-line for the count-down timer
+   * Starts the timer time-line for the count-down timer
    */
   private void startTimer() {
 
+    //creates new timeline for the countdown timer
     seconds.set(60);
     timeline = new Timeline();
     timeline.getKeyFrames().add(new KeyFrame(Duration.seconds(60), new KeyValue(seconds, 0)));
+
+    //binds the timer label to the timeline
     lblTime.textProperty().bind(seconds.asString());
     // time line plays once only
     timeline.setCycleCount(1);
---
+++ src/main/java/nz/ac/auckland/se206/SaveMenuController.java
@@ -0,0 +1,112 @@
+package nz.ac.auckland.se206;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javax.imageio.ImageIO;
+import javax.swing.filechooser.FileSystemView;
+
+public class SaveMenuController {
+
+  @FXML
+  private TextField txtFileName;
+
+  @FXML
+  private TextField txtDirectory;
+
+  @FXML
+  private Button btnSave;
+
+  @FXML
+  private Button btnCancel;
+
+  private BufferedImage image;
+
+  private Stage stage;
+
+  public void setImage(BufferedImage image) {
+    this.image = image;
+  }
+
+  public void setStage(Stage stage) {
+    this.stage = stage;
+  }
+
+  @FXML
+  private void onSave() throws IOException {
+
+    // set the default values for the directory and name, use desktop as the default
+    // directory
+    String directory = FileSystemView.getFileSystemView().getHomeDirectory().getAbsolutePath();
+    String name = "default" + System.currentTimeMillis();
+
+    // check if any of the text fields are filled up, if filled up, overwrite the
+    // default values
+    if (!txtDirectory.getText().equalsIgnoreCase("")) {
+      directory = txtDirectory.getText();
+    }
+    if (!txtFileName.getText().equalsIgnoreCase("")) {
+      name = txtFileName.getText();
+    }
+
+    // create a file for the canvas drawing
+    File createdFile = saveCurrentSnapshotOnFile();
+
+    // this file input stream instance reads the created file into
+    // bytes so that the file output stream can write it
+    FileInputStream fis = new FileInputStream(createdFile);
+    byte[] arr = fis.readAllBytes();
+    fis.close();
+
+    // create the file path for the saved canvas drawing
+    Path newFilePath = Paths.get(directory + "/" + name + ".bmp");
+    Files.createFile(newFilePath); // create a file in this file path
+
+    // initialise the output stream and write on the created file
+    FileOutputStream fos = new FileOutputStream(directory + "/" + name + ".bmp");
+    fos.write(arr);
+    fos.close();
+
+    this.stage.close();
+
+  }
+
+  @FXML
+  private void onCancel() {
+    this.stage.close();
+  }
+
+  /**
+   * Save the current snapshot on a bitmap file.
+   *
+   * @return The file of the saved image.
+   * @throws IOException If the image cannot be saved.
+   */
+  private File saveCurrentSnapshotOnFile() throws IOException {
+    // You can change the location as you see fit.
+    final File tmpFolder = new File("tmp");
+
+    if (!tmpFolder.exists()) {
+      tmpFolder.mkdir();
+    }
+
+    // We save the image to a file in the tmp folder.
+    final File imageToClassify = new File(tmpFolder.getName() + "/snapshot" + System.currentTimeMillis() + ".bmp");
+
+    // Save the image to a file.
+    ImageIO.write(image, "bmp", imageToClassify);
+
+    return imageToClassify;
+  }
+
+}
