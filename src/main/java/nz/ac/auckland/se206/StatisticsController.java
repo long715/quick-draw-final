6d9b2dc7c3ae7b80d16cb4a469eadb040a71a720
Bloooey <bluey17501@gmail.com>
2022-09-14 23:49:23 +1200

    [fix] added back button to statistics screen

--- src/main/java/nz/ac/auckland/se206/StatisticsController.java
+++ src/main/java/nz/ac/auckland/se206/StatisticsController.java
@@ -2,6 +2,7 @@ package nz.ac.auckland.se206;
 
 import javafx.fxml.FXML;
 import javafx.scene.chart.PieChart;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class StatisticsController {
@@ -9,6 +10,7 @@ public class StatisticsController {
   @FXML private Label lblBestTime;
   @FXML private Label lblBestWord;
   @FXML private PieChart pieChart;
+  @FXML private Button btnBack;
 
   @FXML
   private void initialize() {
@@ -26,5 +28,10 @@ public class StatisticsController {
       pieChart.getData().add(slice1);
       pieChart.getData().add(slice2);
     }
+  }
+
+  @FXML 
+  private void onBack() {
+    btnBack.getScene().setRoot(SceneManager.getUi(SceneManager.AppUi.MENU)); //returns to menu
   }
 }
