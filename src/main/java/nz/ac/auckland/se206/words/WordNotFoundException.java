e81a9c28a5888256c3b2073f680e898ea0419523
long715 <long715@aucklanduni.ac.nz>
2022-10-16 16:11:06 +1300

    [style] JavaDocs and throws description for other classes in other packages

--- src/main/java/nz/ac/auckland/se206/words/WordNotFoundException.java
+++ src/main/java/nz/ac/auckland/se206/words/WordNotFoundException.java
@@ -6,7 +6,13 @@ public class WordNotFoundException extends Exception {
   private String word;
   private String subMessage;
 
-  // constructor for the exception
+  /**
+   * The constructor for the Custom exception which initialises the Exception message.
+   *
+   * @param word The word which matches no definitions
+   * @param message The main message for the exception
+   * @param subMessage The sub message for the exception
+   */
   WordNotFoundException(String word, String message, String subMessage) {
     super(message);
     this.word = word;
